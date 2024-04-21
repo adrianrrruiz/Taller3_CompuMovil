c8525b995b0ea18abbd1738576eb7ba46197a414
Adrian Ruiz <adrianruiz0208@gmail.com>
2024-04-21 16:19:12 -0500

    Some layouts ready

---
+++ app/src/main/java/com/example/taller3/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.taller3
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.taller3.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginbutton.setOnClickListener { startActivity(Intent(this, LoginActivity::class.java)) }
+        binding.registerbutton.setOnClickListener { startActivity(Intent(this, RegisterActivity::class.java)) }
+    }
+}
